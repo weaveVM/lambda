f0e71d4b28a917c1bec6dcbbbfbe84a9891d3696
charmful0x <charmful0x@gmail.com>
2024-06-12 10:51:29 +0200

    feat: encode/decode functions

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,27 @@ use neon::prelude::*;
 use crate::utils::ethabiwrapper::{encode_ethabi, decode_ethabi};
 mod utils;
 
-fn test_encode(mut cx: FunctionContext) -> JsResult<(JsString)> {
+fn rs_encode(mut cx: FunctionContext) -> JsResult<(JsString)> {
     let input = cx
     .argument::<JsString>(0)? 
     .value(&mut cx);
 
     let encode_res: String = encode_ethabi(&input).unwrap();
-	let decode_res: String = decode_ethabi(&encode_res).unwrap();
-
-    let result = format!("{} {}", encode_res, decode_res);
-	println!("{}", result);
-    Ok(cx.string(result))
+    Ok(cx.string(encode_res))
 }
 
+fn rs_decode(mut cx: FunctionContext) -> JsResult<(JsString)> {
+    let input = cx
+    .argument::<JsString>(0)? 
+    .value(&mut cx);
+
+	let decode_res: String = decode_ethabi(&input).unwrap();
+    Ok(cx.string(decode_res))
+}
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("test_encode", test_encode)?;
+    cx.export_function("rs_decode", rs_decode)?;
+    cx.export_function("rs_encode", rs_encode)?;
     Ok(())
 }
