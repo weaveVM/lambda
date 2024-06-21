03113f3f34505df3d679a979e42a89f72cd091e1
charmful0x <charmful0x@gmail.com>
2024-06-22 01:58:59 +0200

    feat: add irys connection

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,2 +1,3 @@
 export const WVM_RPC_URL = "https://testnet-rpc.wvm.dev";
+export const IRYS_EVM_RPC_URL = "https://ethereum-sepolia-rpc.publicnode.com";
 export const SEQUENCER_ADDRESS = "0x197f818c1313dc58b32d88078ecdfb40ea822614";
