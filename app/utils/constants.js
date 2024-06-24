de100818f73a91cb20c0ad9b471cac412346f1bb
charmful0x <charmful0x@gmail.com>
2024-06-24 12:42:20 +0200

    feat: PoC

--- app/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const WVM_RPC_URL = "https://testnet-rpc.wvm.dev";
-export const IRYS_EVM_RPC_URL = "https://ethereum-sepolia-rpc.publicnode.com";
-export const SEQUENCER_ADDRESS = "0x197f818c1313DC58b32D88078ecdfB40EA822614";
-export const MEM_SIMULATE_ENDPOINT = `https://mem-testnet-bfdc8ff3530f.herokuapp.com/`;
