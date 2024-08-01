a820b2219fdda1880db2165831e97fc77a599997
Darwin <charmful0x@gmail.com>
2024-08-01 18:43:18 +0200

    chore: fix deploy-contract example

--- examples/deploy-contract.js
+++ examples/deploy-contract.js
@@ -2,6 +2,8 @@ import { ethers } from "ethers";
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
+
+const SEQUENCER_ADDRESS = "0x197f818c1313dc58b32d88078ecdfb40ea822614";
 const WVM_RPC_URL= "https://testnet-rpc.wvm.dev"
 const SEQUENCER_ENDPOINT = "https://wvm-lambda-0755acbdae90.herokuapp.com";
 
@@ -57,7 +59,7 @@ function prepareContractData() {
           }`.split("").map((char) => char.charCodeAt(0))
     }
 
-    return JSON.stringify(res);
+    return JSON.stringify(transaction);
 }
 
 /**
