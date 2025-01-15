ea50d85a99f5062a4b2b7c99981cf7b64543670d
Rani <charmful0x@gmail.com>
2025-01-15 10:17:05 +0100

    chore: change sequencer address

--- sequencer/utils/constants.js
+++ sequencer/utils/constants.js
@@ -1,4 +1,4 @@
 export const WVM_RPC_URL = "https://testnet-rpc.wvm.dev";
 export const IRYS_EVM_RPC_URL = "https://ethereum-sepolia-rpc.publicnode.com";
-export const SEQUENCER_ADDRESS = "0x197f818c1313DC58b32D88078ecdfB40EA822614";
+export const SEQUENCER_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const MEM_SIMULATE_ENDPOINT = "https://mem-testnet-bfdc8ff3530f.herokuapp.com/";
